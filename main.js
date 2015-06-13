90b19f347458883cb335bf5eebcd8c4c0fe5ed46
BrianCottrell <Brian_Cottrell@inbox.com>
2015-06-13 16:10:03 -0700

    Added image url to text feature.

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "https://api.idolondemand.com/1/api/sync/querytextindex/v1?text=pizza&apikey=66c1a05f-e956-426f-a0e0-2c2f3756423f&max_page_results=1&summary=quick";
+document.getElementsByClassName('url-button')[0].addEventListener('click', function(){
+	var xmlhttp = new XMLHttpRequest();
+	var url = "https://api.idolondemand.com/1/api/sync/ocrdocument/v1?apikey=66c1a05f-e956-426f-a0e0-2c2f3756423f&url="+document.getElementsByClassName('url-input')[0].value;
 
-xmlhttp.onreadystatechange = function() {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    	var response = JSON.parse(xmlhttp.responseText);
-        console.log(response.documents[0].summary);
-    }
-}
-xmlhttp.open("GET", url, true);
-xmlhttp.send();
+	xmlhttp.onreadystatechange = function() {
+	    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+	    	var response = JSON.parse(xmlhttp.responseText);
+	        document.getElementsByClassName('image-text')[0].innerHTML = response.text_block[0].text;
+	    }
+	}
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+});
+
+// var xmlhttp = new XMLHttpRequest();
+// var url = "https://api.idolondemand.com/1/api/sync/querytextindex/v1?apikey=66c1a05f-e956-426f-a0e0-2c2f3756423f&max_page_results=1&summary=quick&text=pizza";
+
+// xmlhttp.onreadystatechange = function() {
+//     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//     	var response = JSON.parse(xmlhttp.responseText);
+//         console.log(response.documents[0].summary);
+//     }
+// }
+// xmlhttp.open("GET", url, true);
+// xmlhttp.send();
